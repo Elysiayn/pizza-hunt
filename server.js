95c2450456a9ffffe2eab8221cdf137c85a7cb90
Elysiayn <wendy.lemus.wl@gmail.com>
2023-03-17 22:19:54 -0400

    fixed local server deployment issue

--- server.js
+++ server.js
@@ -2,17 +2,18 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
-const PORT = process.env.PORT || 27017;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pizza-hunt', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
   useFindAndModify: false,
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  family:4,
 });
 
 // Use this to log mongo queries being executed!
